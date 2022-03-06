ccf18bf5d22d13dce0b075cd68af36bf163e2e1b
Max Malevich <malevich.m.439-1@e.tusur.ru>
2022-03-06 20:20:44 +0700

    good_scale

--- src/Components/appHeader/AppHeader.js
+++ src/Components/appHeader/AppHeader.js
@@ -4,9 +4,8 @@ import logo from '../../resources/logo.png'
 const AppHeader = () => {
     return(
         <header className="app__header">
-            <h1 className="app__title">
                 <img className='app__logo' src={logo} alt="Молекулярная кухня" />
-            </h1>
+                <button className="app__button">Заказать сейчас</button>
         </header>
     )
 }
